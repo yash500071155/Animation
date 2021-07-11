1981defed91d6ebfc2a34cdc196ada34bad3e2ec
yash500071155 <75775134+yash500071155@users.noreply.github.com>
2021-07-11 13:01:53 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var audio=document.createElement('Audio');
+
+audio.setAttribute('src','Amplifier.mp3');
+// audio.setAttribute('src','sound.mp3');
+audio.loop=true;
+
+    audio.play();
+
+    
+      
+
